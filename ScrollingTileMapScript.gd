8e9cef24e9d1c7e6a8350ea83a20aa0780aa1824
LDGamingExpress <110698883+LDGamingExpress@users.noreply.github.com>
2026-02-02 22:52:55 -0500

    Scrolling Tile Maps
    Scrolling tilemaps can now be used for continental, desert, and arctic worlds

---
+++ ScrollingTileMapScript.gd
@@ -0,0 +1,204 @@
+extends Node2D
+var Biome = "Continental"
+var Tiles = []
+var TileBiomes = []
+var WorldWidth = 10
+var WorldHeight = 10
+
+func _ready() -> void:
+	WorldHeight = int(Globals.ScreenSize.y * 1.2 / 32 + 1)
+	WorldWidth = int(Globals.ScreenSize.x * 1.2 / 32 + 1)
+	
+	
+	for y in range(0, WorldHeight):
+		var TempMat = []
+		var BiomeMat = []
+		for x in range(0, WorldWidth):
+			if y == 0 and x == 0:
+				TempMat.append(randi_range(0,4))
+			else:
+				if y == 0:
+					var AllowedTypes = []
+					match TempMat[x - 1]:
+						0:
+							AllowedTypes = [0,1,1,0]
+						1:
+							AllowedTypes = [0,1,2,1,2]
+						2:
+							AllowedTypes = [1,2,3,1,2]
+						3:
+							AllowedTypes = [2,3,4,2]
+						4:
+							AllowedTypes = [3,4]
+					TempMat.append(AllowedTypes.pick_random())
+				else:
+					if x == 0:
+						var AllowedTypes = []
+						match Tiles[y - 1][0]:
+							0:
+								AllowedTypes = [0,1,1,0]
+							1:
+								AllowedTypes = [0,1,2,1,2]
+							2:
+								AllowedTypes = [1,2,3,1,2]
+							3:
+								AllowedTypes = [2,3,4,2]
+							4:
+								AllowedTypes = [3,4]
+						TempMat.append(AllowedTypes.pick_random())
+					else:
+						var AllowedTypes = []
+						match Tiles[y - 1][x]:
+							0:
+								AllowedTypes = [0,1,1,0]
+							1:
+								AllowedTypes = [0,1,2,1,2]
+							2:
+								AllowedTypes = [1,2,3,1,2]
+							3:
+								AllowedTypes = [2,3,4,2]
+							4:
+								AllowedTypes = [3,4]
+						match TempMat[x - 1]:
+							0:
+								AllowedTypes.erase(2)
+								AllowedTypes.erase(3)
+								AllowedTypes.erase(4)
+								AllowedTypes.append(0)
+								AllowedTypes.append(1)
+								AllowedTypes.append(0)
+								AllowedTypes.append(1)
+							1:
+								AllowedTypes.erase(3)
+								AllowedTypes.erase(4)
+								AllowedTypes.append(0)
+								AllowedTypes.append(1)
+								AllowedTypes.append(2)
+								AllowedTypes.append(1)
+								AllowedTypes.append(2)
+							2:
+								AllowedTypes.erase(0)
+								AllowedTypes.erase(4)
+								AllowedTypes.append(1)
+								AllowedTypes.append(2)
+								AllowedTypes.append(3)
+								AllowedTypes.append(1)
+								AllowedTypes.append(2)
+							3:
+								AllowedTypes.erase(0)
+								AllowedTypes.erase(1)
+								AllowedTypes.append(2)
+								AllowedTypes.append(3)
+								AllowedTypes.append(4)
+								AllowedTypes.append(2)
+							4:
+								AllowedTypes.erase(0)
+								AllowedTypes.erase(1)
+								AllowedTypes.erase(2)
+								AllowedTypes.append(3)
+								AllowedTypes.append(4)
+						TempMat.append(AllowedTypes.pick_random())
+			BiomeMat.append(Biome)
+		Tiles.append(TempMat)
+		TileBiomes.append(BiomeMat)
+	Redraw()
+
+func Redraw():
+	for y in range(0, WorldHeight):
+		for x in range(0, WorldWidth):
+			var BiomeOffset = 0
+			match TileBiomes[y][x]:
+				"Continental":
+					BiomeOffset = 0
+				"Desert":
+					BiomeOffset = 3
+				"Arctic":
+					BiomeOffset = 6
+			$TileMapLayer.set_cell(Vector2i(x - WorldWidth/2,y - WorldHeight/2),0,Vector2i(Tiles[y][x],BiomeOffset + randi_range(0,2)))
+
+func _physics_process(delta: float) -> void:
+	$TileMapLayer.position.y += Globals.PlayerSpeed*delta
+	if $TileMapLayer.position.y > 32.0:
+		$TileMapLayer.position.y -= 32.0
+		
+		# Shift:
+		var y = WorldHeight - 1
+		while y > 0:
+			Tiles[y] = Tiles[y - 1]
+			TileBiomes[y] = TileBiomes[y - 1]
+			y -= 1
+		
+		# Generate new row:
+		var TempMat = []
+		var BiomeMat = []
+		for x in range(0, WorldWidth):
+			if x == 0:
+				var AllowedTypes = []
+				match Tiles[1][0]:
+					0:
+						AllowedTypes = [0,1,1,0]
+					1:
+						AllowedTypes = [0,1,2,1,2]
+					2:
+						AllowedTypes = [1,2,3,1,2]
+					3:
+						AllowedTypes = [2,3,4,2]
+					4:
+						AllowedTypes = [3,4]
+				TempMat.append(AllowedTypes.pick_random())
+			else:
+				var AllowedTypes = []
+				match Tiles[1][x]:
+					0:
+						AllowedTypes = [0,1,1,0]
+					1:
+						AllowedTypes = [0,1,2,1,2]
+					2:
+						AllowedTypes = [1,2,3,1,2]
+					3:
+						AllowedTypes = [2,3,4,2]
+					4:
+						AllowedTypes = [3,4]
+				match TempMat[x - 1]:
+					0:
+						AllowedTypes.erase(2)
+						AllowedTypes.erase(3)
+						AllowedTypes.erase(4)
+						AllowedTypes.append(0)
+						AllowedTypes.append(1)
+						AllowedTypes.append(0)
+						AllowedTypes.append(1)
+					1:
+						AllowedTypes.erase(3)
+						AllowedTypes.erase(4)
+						AllowedTypes.append(0)
+						AllowedTypes.append(1)
+						AllowedTypes.append(2)
+						AllowedTypes.append(1)
+						AllowedTypes.append(2)
+					2:
+						AllowedTypes.erase(0)
+						AllowedTypes.erase(4)
+						AllowedTypes.append(1)
+						AllowedTypes.append(2)
+						AllowedTypes.append(3)
+						AllowedTypes.append(1)
+						AllowedTypes.append(2)
+					3:
+						AllowedTypes.erase(0)
+						AllowedTypes.erase(1)
+						AllowedTypes.append(2)
+						AllowedTypes.append(3)
+						AllowedTypes.append(4)
+						AllowedTypes.append(2)
+					4:
+						AllowedTypes.erase(0)
+						AllowedTypes.erase(1)
+						AllowedTypes.erase(2)
+						AllowedTypes.append(3)
+						AllowedTypes.append(4)
+				TempMat.append(AllowedTypes.pick_random())
+			BiomeMat.append(Biome)
+		Tiles[0] = TempMat
+		TileBiomes[0] = BiomeMat
+		Redraw()
