104ffcce8622a079a3c58ab8bdec84d94096de88
LDGamingExpress <110698883+LDGamingExpress@users.noreply.github.com>
2026-02-04 22:25:29 -0500

    Updated procedural generation
    The chances for tiles can now be more easily edited by adding or subtracting amounts of them in each of the rule arrays

--- ScrollingTileMapScript.gd
+++ ScrollingTileMapScript.gd
@@ -4,6 +4,13 @@ var Tiles = []
 var TileBiomes = []
 var WorldWidth = 10
 var WorldHeight = 10
+var TileAllow = [
+	[0,1],
+	[0,1,2,1,2],
+	[1,2,3,1,2,2,1,2,1,2,1,2],
+	[2,3,4,2,2],
+	[3,4,4,4,4,4,4]
+]
 
 func _ready() -> void:
 	WorldHeight = int(Globals.ScreenSize.y * 1.2 / 32 + 1)
@@ -18,85 +25,26 @@ func _ready() -> void:
 				TempMat.append(randi_range(0,4))
 			else:
 				if y == 0:
-					var AllowedTypes = []
-					match TempMat[x - 1]:
-						0:
-							AllowedTypes = [0,1,1,0]
-						1:
-							AllowedTypes = [0,1,2,1,2]
-						2:
-							AllowedTypes = [1,2,3,1,2]
-						3:
-							AllowedTypes = [2,3,4,2]
-						4:
-							AllowedTypes = [3,4]
+					var AllowedTypes = TileAllow[TempMat[x - 1]].duplicate_deep()
 					TempMat.append(AllowedTypes.pick_random())
 				else:
 					if x == 0:
-						var AllowedTypes = []
-						match Tiles[y - 1][0]:
-							0:
-								AllowedTypes = [0,1,1,0]
-							1:
-								AllowedTypes = [0,1,2,1,2]
-							2:
-								AllowedTypes = [1,2,3,1,2]
-							3:
-								AllowedTypes = [2,3,4,2]
-							4:
-								AllowedTypes = [3,4]
+						var AllowedTypes = TileAllow[Tiles[y - 1][0]].duplicate_deep()
 						TempMat.append(AllowedTypes.pick_random())
 					else:
-						var AllowedTypes = []
-						match Tiles[y - 1][x]:
-							0:
-								AllowedTypes = [0,1,1,0]
-							1:
-								AllowedTypes = [0,1,2,1,2]
-							2:
-								AllowedTypes = [1,2,3,1,2]
-							3:
-								AllowedTypes = [2,3,4,2]
-							4:
-								AllowedTypes = [3,4]
-						match TempMat[x - 1]:
-							0:
-								AllowedTypes.erase(2)
-								AllowedTypes.erase(3)
-								AllowedTypes.erase(4)
-								AllowedTypes.append(0)
-								AllowedTypes.append(1)
-								AllowedTypes.append(0)
-								AllowedTypes.append(1)
-							1:
-								AllowedTypes.erase(3)
-								AllowedTypes.erase(4)
-								AllowedTypes.append(0)
-								AllowedTypes.append(1)
-								AllowedTypes.append(2)
-								AllowedTypes.append(1)
-								AllowedTypes.append(2)
-							2:
-								AllowedTypes.erase(0)
-								AllowedTypes.erase(4)
-								AllowedTypes.append(1)
-								AllowedTypes.append(2)
-								AllowedTypes.append(3)
-								AllowedTypes.append(1)
-								AllowedTypes.append(2)
-							3:
-								AllowedTypes.erase(0)
-								AllowedTypes.erase(1)
-								AllowedTypes.append(2)
-								AllowedTypes.append(3)
-								AllowedTypes.append(4)
-								AllowedTypes.append(2)
-							4:
-								AllowedTypes.erase(0)
-								AllowedTypes.erase(1)
-								AllowedTypes.erase(2)
-								AllowedTypes.append(3)
-								AllowedTypes.append(4)
+						var AllowedTypes = TileAllow[Tiles[y - 1][x]].duplicate_deep()
+						var NewAllowedTypes = TileAllow[TempMat[x - 1]].duplicate_deep()
+						var i = 0
+						#print(TileAllow)
+						while i < len(AllowedTypes) and len(AllowedTypes) > 0:
+							#print(AllowedTypes)
+							if !NewAllowedTypes.has(AllowedTypes[i]):
+								AllowedTypes.erase(AllowedTypes[i])
+								i -= 1
+							i += 1
+							#print(i)
+							#print(len(AllowedTypes))
+						AllowedTypes.append_array(NewAllowedTypes)
 						TempMat.append(AllowedTypes.pick_random())
 			BiomeMat.append(Biome)
 		Tiles.append(TempMat)
