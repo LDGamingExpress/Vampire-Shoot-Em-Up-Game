980866127f93a7f4df8c19969502a0265aa50bef
LDGamingExpress <110698883+LDGamingExpress@users.noreply.github.com>
2026-02-07 21:19:59 -0500

    Bat Enemies
    Bat Enemies Have Been Implemented that will attack similar to asteroids but can come from the sides too

---
+++ BatScript.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+var Team = "Enemy"
+# "Player", "Enemy", "Ally"
+var Damage = 1
+var Speed = Globals.PlayerSpeed + randi_range(-5,5) + 100
+var ExplosionParticles = load("res://ExplosionParticles.tscn")
+var Health = 1
+
+func _ready() -> void:
+	velocity = Vector2(Speed * cos(rotation),Speed * sin(rotation))
+	#$AnimatedSprite2D.rotation = randf_range(0,2*PI)
+	await get_tree().create_timer(0.1).timeout
+	$GPUParticles2D.emitting = true
+	
+
+func _physics_process(delta: float) -> void:
+	move_and_slide()
+	if position.y > Globals.ScreenSize.y/2:
+		$GPUParticles2D.emitting = false
+		queue_free()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.Team != "Enemy":
+		body.Health -= Damage
+		body.Damaged()
+		var newObj = ExplosionParticles.instantiate()
+		newObj.position = position
+		get_parent().add_child(newObj)
+		$GPUParticles2D.emitting = false
+		queue_free()
+
+func Damaged():
+	if Health <= 0:
+		$GPUParticles2D.emitting = false
+		queue_free()
--- MainGameScript.gd
+++ MainGameScript.gd
@@ -1,9 +1,11 @@
 extends Node2D
-var LevelStageEnemies = [["Asteroid",3,30,2.0],["Asteroid",10,100,3.0]]
+var LevelStageEnemies = [["Asteroid",3,3,2.0],["Bat",5,50,1.5],["BatL",5,50,1.5],["BatR",5,50,1.5],["Asteroid",10,100,3.0]]
 # Format: ["Enemy Type", Enemies spawned in each wave, total enemies needed, time between waves]
+var LevelBackground = ["Space","Space"]
 var CurrentEnemiesDone = 0
 var Stage = 0
 var Asteroid = load("res://Asteroid.tscn")
+var Bat = load("res://Bat.tscn")
 
 func _ready() -> void:
 	await get_tree().create_timer(LevelStageEnemies[0][3]).timeout
@@ -12,12 +14,25 @@ func _ready() -> void:
 func EnemySpawner():
 	if CurrentEnemiesDone >= LevelStageEnemies[Stage][2]:
 		Stage += 1
+		CurrentEnemiesDone = 0
 	if Stage < len(LevelStageEnemies):
 		match LevelStageEnemies[Stage][0]:
 			"Asteroid":
 				for i in range(0,LevelStageEnemies[Stage][1]):
 					CurrentEnemiesDone += 1
 					SpawnAsteroid()
+			"Bat":
+				for i in range(0,LevelStageEnemies[Stage][1]):
+					CurrentEnemiesDone += 1
+					SpawnBat()
+			"BatL":
+				for i in range(0,LevelStageEnemies[Stage][1]):
+					CurrentEnemiesDone += 1
+					SpawnBatL()
+			"BatR":
+				for i in range(0,LevelStageEnemies[Stage][1]):
+					CurrentEnemiesDone += 1
+					SpawnBatR()
 		await get_tree().create_timer(LevelStageEnemies[Stage][3]).timeout
 		EnemySpawner()
 
@@ -25,4 +40,22 @@ func SpawnAsteroid():
 	var newObj = Asteroid.instantiate()
 	newObj.position = Vector2(randf_range(-Globals.ScreenSize.x/2.0,Globals.ScreenSize.x/2.0),-Globals.ScreenSize.y/2)
 	newObj.rotation = PI/2
+	call_deferred("add_child",newObj)
+
+func SpawnBat():
+	var newObj = Bat.instantiate()
+	newObj.position = Vector2(randf_range(-Globals.ScreenSize.x/2.0,Globals.ScreenSize.x/2.0),-Globals.ScreenSize.y/2)
+	newObj.rotation = PI/2
+	call_deferred("add_child",newObj)
+
+func SpawnBatL():
+	var newObj = Bat.instantiate()
+	newObj.position = Vector2(randf_range(-Globals.ScreenSize.x/2.0,0),-Globals.ScreenSize.y/2)
+	newObj.rotation = PI/4
+	call_deferred("add_child",newObj)
+
+func SpawnBatR():
+	var newObj = Bat.instantiate()
+	newObj.position = Vector2(randf_range(0,Globals.ScreenSize.x/2.0),-Globals.ScreenSize.y/2)
+	newObj.rotation = 3*PI/4
 	call_deferred("add_child",newObj)
